1478e7d51bde53422d9f4a9dafe86da59c096d56
Polina Popova <kiwi-ya@ya.ru>
2018-03-14 23:24:34 +0300

    deleted unnecessary (add contact)

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -12,15 +11,13 @@ def is_alert_present(wd):
 
 class test_add_contact(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
     def test_test_add_contact(self):
         success = True
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -39,18 +36,13 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys("las1")
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("nick")
-        wd.find_element_by_name("theform").click()
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
         wd.find_element_by_name("nickname").send_keys("nic1")
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
         wd.find_element_by_name("title").send_keys("tit1")
-        wd.find_element_by_name("address").click()
+        # wd.find_element_by_name("address").click()
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
         wd.find_element_by_name("company").send_keys("com1")
@@ -59,25 +51,16 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("address").send_keys("add1")
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("hom1")
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
         wd.find_element_by_name("home").send_keys("111")
-        wd.find_element_by_name("theform").click()
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
         wd.find_element_by_name("mobile").send_keys("222")
-        wd.find_element_by_name("fax").click()
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
         wd.find_element_by_name("work").send_keys("333")
-        wd.find_element_by_name("theform").click()
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
         wd.find_element_by_name("fax").send_keys("444")
-        wd.find_element_by_name("theform").click()
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys("a1@a.ru")
@@ -106,9 +89,6 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("ayear").send_keys("81")
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("add1")
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
         wd.find_element_by_name("address2").send_keys("add2")
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
@@ -119,10 +99,6 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         wd.find_element_by_link_text("home page").click()
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
         self.assertTrue(success)
     
     def tearDown(self):
