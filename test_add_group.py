0d3e307e50d36a85e50117d8f6f3127ae5537962
Polina Popova <kiwi-ya@ya.ru>
2018-03-17 21:02:28 +0300

    small method to big method (script add gpoup)

--- test_add_group.py
+++ test_add_group.py
@@ -17,20 +17,14 @@ class test_add_group(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups(wd)
         self.create_group(wd, Group(name="grup1", header="h1", footer="f1"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -41,6 +35,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_groups(wd)
         # init gpoup
         wd.find_element_by_name("new").click()
         # fill form
@@ -55,11 +50,13 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def open_groups(self, wd):
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
