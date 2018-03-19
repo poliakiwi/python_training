0f185bf46656546900d04a363171af5801017f94
Polina Popova <kiwi-ya@ya.ru>
2018-03-20 01:23:42 +0300

    decomposition app (add session)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,18 +12,18 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.fill_contact(Contact(fir="fir1", mid="mid1", las="las1", nic="nic1", tit="tit1", com="com1", add_1="add1",
                  tel_1="111", tel_2="222", tel_3="333", tel_4="444", mail_1="a1@a.ru", mail_2="a2@a.ru",
                  mail_3="a3@a.ru", hom="hom1.ru", add_2="add2", hom_2="hom2", not_2="not2"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.fill_contact(Contact(fir="", mid="", las="", nic="", tit="", com="", add_1="",
                  tel_1="", tel_2="", tel_3="", tel_4="", mail_1="", mail_2="",
                  mail_3="", hom="", add_2="", hom_2="", not_2=""))
-    app.logout()
+    app.session.logout()
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,14 +12,14 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="grup1", header="h1", footer="f1"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
