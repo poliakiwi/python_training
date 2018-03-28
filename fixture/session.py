0e6d3c131c3d01722402316faa12311d37345ba3
Polina Popova <kiwi-ya@ya.ru>
2018-03-29 00:38:59 +0300

    add ensure login and logout

--- fixture/session.py
+++ fixture/session.py
@@ -17,6 +17,28 @@ class SessionHelper:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logout():
+            if self.is_login_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logout():
+            self.logout()
+
+    def is_logout(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_login_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "(" + username + ")"
