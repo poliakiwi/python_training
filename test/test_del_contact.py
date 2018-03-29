55ab953593e1e33747febe00002a6e866c263d3c
Polina Popova <kiwi-ya@ya.ru>
2018-03-29 17:21:42 +0300

    add validation for contacts

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,6 @@
 from model.contacts import Contact
 
 def test_del_first_contact(app):
+    if app.contacts.count() == 0:
+        app.contacts.create(Contact(fir="Test"))
     app.contacts.del_first()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,4 +1,6 @@
 from model.contacts import Contact
 
 def test_edit_first_contact_name(app):
+    if app.contacts.count() == 0:
+        app.contacts.create(Contact(fir="Test"))
     app.contacts.edit_first(Contact(fir="changed firstname"))
