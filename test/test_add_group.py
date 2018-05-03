8e0cb46a9d88a5ac9ee8ceb21323ac850f9853f8
Polina Popova <kiwi-ya@ya.ru>
2018-05-04 01:47:04 +0300

    parametrize tests for group + add random

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_group(app):
+def random_str(prefix, maxlen):
+    sym = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(sym) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_str("NN",10), header=random_str("HH",20), footer=random_str("FF",20))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("gr", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, gr):
     old_groups = app.groups.get_groups_list()
-    newG = Group(name="grup1", header="h1", footer="f1")
-    app.groups.create(newG)
+    app.groups.create(gr)
     new_groups = app.groups.get_groups_list()
     assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(newG)
+    old_groups.append(gr)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_empty_group(app):
-    old_groups = app.groups.get_groups_list()
-    newG=Group(name="", header="", footer="")
-    app.groups.create(newG)
-    new_groups = app.groups.get_groups_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(newG)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-
