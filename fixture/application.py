3e1f5c5825b7fb7946fec337639650dfbafba68b
Polina Popova <kiwi-ya@ya.ru>
2018-05-04 13:19:35 +0300

    add parameters (browser, url)

--- fixture/application.py
+++ fixture/application.py
@@ -1,20 +1,29 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("unidentified browser - %s" % browser )
+
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.groups = GroupHelper(self)
         self.contacts = ContactHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def click_to_home_page(self):
         wd = self.wd
