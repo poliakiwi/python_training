326943e2899c09d38fb0af893f85f265b6e971f4
Polina Popova <kiwi-ya@ya.ru>
2018-05-11 00:39:47 +0300

    modify to get group from db (add groups)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,13 @@ from model.group import Group
 import re
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     gr = json_groups
-    old_groups = app.groups.get_groups_list()
+    old_groups = db.get_group_list()
     app.groups.create(gr)
-    new_groups = app.groups.get_groups_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    gr.name = clear_blank(gr.name)
+    new_groups = db.get_group_list()
+    # assert len(old_groups) + 1 == len(new_groups)
+    # gr.name = clear_blank(gr.name)
     old_groups.append(gr)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
