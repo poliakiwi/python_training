64e371da9a598ee4cf61296ab4d62585f40cde11
Polina Popova <kiwi-ya@ya.ru>
2018-05-11 01:42:09 +0300

    modify to add check_ui (del groups)

--- test/test_del_gpoup.py
+++ test/test_del_gpoup.py
@@ -1,7 +1,9 @@
 from model.group import Group
 import random
+import re
 
-def test_del_some_group(app, db):
+
+def test_del_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.groups.create(Group(name="Test"))
     old_groups = db.get_group_list()
@@ -11,3 +13,11 @@ def test_del_some_group(app, db):
     assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
     assert old_groups == new_groups
+    def clean(group):
+        group.name = re.sub(" +", " ", group.name)
+        return Group(id=group.id, name=group.name.strip())
+    if check_ui:
+        ui_list = app.groups.get_groups_list()
+        db_list = map(clean, new_groups)
+        assert sorted(db_list, key=Group.id_or_max) == sorted(ui_list, key=Group.id_or_max)
+
